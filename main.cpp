038c5fb8a91feef3714808ce7174391d874b4279
fixbug <3458501995@qq.com>
2025-06-07 17:16:10 +0800

    测试分支

--- main.cpp
+++ main.cpp
@@ -13,4 +13,37 @@ int main()
 	cout << "010" << endl;
 
 	// windows 你好
+
+	void sort(int arr[], int i, int j)
+	{
+		int val = arr[i];
+		int l = i;
+		int r = j;
+		while (l < r)
+		{
+			while (l<r && arr[r] >= val)
+			{
+				r--;
+			}
+			if (l < r)
+			{
+				arr[l++] = arr[r];
+			}
+			while (l < r && arr[l] <= val)
+			{
+				l++;
+			}
+			if (l < r)
+			{
+				arr[r--] = arr[l];
+			}
+		}
+		arr[l] = val;
+		sort(arr, i, l - 1);
+		sort(arr, l + 1, j);
+	}
+	void sort(int arr[], int size)
+	{
+		sort(arr, 0, size);
+	}
 }
