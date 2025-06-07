9c7e51bbea06e180a3f6a494a4984dc205b21dba
linux_1_ubuntu fixbug <3458501995@qq.com>
2025-06-07 08:18:37 +0000

    小UB

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {       // ubuntu æ·»åŠ æ³¨é‡Š
 	int arr[] = {12,4,33};
-	cout << "000" << endl;
+	bool flag = false;
+	
+	
+	cout << "010" << endl;
 	// windows Ìí¼ÓµÄ×¢ÊÍ
 }
