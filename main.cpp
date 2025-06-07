4f2f3795df79eeac0991c7e72dcd34cfa486d27a
linux_1_ubuntu fixbug <3458501995@qq.com>
2025-06-07 08:51:06 +0000

    小U修改乱码

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main()
-{       // ubuntu 娣诲姞娉ㄩ噴
+{       // ubuntu 你好
 	int arr[] = {12,4,33};
 
 	// windows git 再修改
-=======
 	bool flag = false;
 	
 	
 	cout << "010" << endl;
 
-
+	// windows 你好
 }
